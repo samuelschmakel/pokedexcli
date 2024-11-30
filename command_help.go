545bc03b87744df860acce2651a7473c4cb4a159
SamSchmakel28 <samuel.schmakel@gmail.com>
2024-11-29 19:04:46 -0600

    Updated help command by iterating over available commands as oppposed to hard-coding commands

--- command_help.go
+++ command_help.go
@@ -6,8 +6,11 @@ var cliName string = "Pokedex"
 
 func commandHelp() error {
 	fmt.Printf("Welcome to %v! These are the available commands: \n", cliName)
-	fmt.Println("help - Show available commands")
-	fmt.Println("exit - Closes your connection to ", cliName)
 	fmt.Println("")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+
 	return nil
 }
